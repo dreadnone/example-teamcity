591955585753020cf05858084a8ade469280e920
dreadnone <garnirmakaroni@gmail.com>
2026-02-26 21:14:27 +0300

    Fix HelloPlayer.java - remove test code

--- src/main/java/plaindoll/HelloPlayer.java
+++ src/main/java/plaindoll/HelloPlayer.java
@@ -1,14 +1,9 @@
 package plaindoll;
 
 public class HelloPlayer{
-	public static void main(String[] args) {
-		Welcomer welcomer = new Welcomer();
-		System.out.println(welcomer.sayWelcome());
-		System.out.println(welcomer.sayFarewell());
-	}
-}
-@Test
-public void welcomerSaysHunter() {
-    String reply = welcomer.getHunterReply();
-    assertTrue(reply.contains("hunter"));
+        public static void main(String[] args) {
+                Welcomer welcomer = new Welcomer();
+                System.out.println(welcomer.sayWelcome());
+                System.out.println(welcomer.sayFarewell());
+        }
 }
