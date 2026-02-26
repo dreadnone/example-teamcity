379ad7e4d1c8e20146945aa537ebf5dfe6c13955
dreadnone <garnirmakaroni@gmail.com>
2026-02-26 21:09:32 +0300

    Fix syntax errors in Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,26 +1,34 @@
 package plaindoll;
 
-public class Welcomer{
-	// Если хочешь больше веселья и информации про ДевОпс - приходи в мои каналы NotOps (telegram, YT, Boosty, Patreon)
-	// https://t.me/notopsofficial
-	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
-	}
-	public String sayFarewell() {
-		return "Farewell, good hunter. May you find your worth in waking world.";
-	}
-	public String sayNeedGold(){
-		return "Not enough gold";
-	}
-	public String saySome(){
-		return "something in the way";
-	}
-}
-public String getHunterReply() {
-    String[] replies = {
+import java.util.Random;
+
+public class Welcomer {
+    
+    private static final String[] HUNTER_REPLIES = {
         "The hunter stalks its prey in the digital jungle",
         "A true hunter never gives up the chase",
         "The night is dark and full of hunters"
     };
-    return replies[new java.util.Random().nextInt(replies.length)];
+    
+    private static final Random RANDOM = new Random();
+    
+    public String sayWelcome() {
+        return "Welcome";
+    }
+    
+    public String sayFarewell() {
+        return "Farewell";
+    }
+    
+    public String sayHunter() {
+        return "Hunter";
+    }
+    
+    public String saySilver() {
+        return "Silver";
+    }
+    
+    public String getHunterReply() {
+        return HUNTER_REPLIES[RANDOM.nextInt(HUNTER_REPLIES.length)];
+    }
 }
